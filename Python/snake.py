4a5aaa1e1e0c3000409bc93df63b2bc0e12defa7
A01781835 <83768910+A01781835@users.noreply.github.com>
2021-05-06 12:03:20 -0500

    Respuesta 1

--- Python/snake.py
+++ Python/snake.py
@@ -3,6 +3,9 @@
 Exercises
 
 1. How do you make the snake faster or slower?
+    The spedd of the snake is controlled inside move, inside ontimer
+    To increase the speed, the number inside ontimer needs to be smaller.
+    To decrease the speed, the number inside ontimer needs to be bigger.
 2. How can you make the snake go around the edges?
 3. How would you move the food?
 4. Change the snake to respond to arrow keys.
