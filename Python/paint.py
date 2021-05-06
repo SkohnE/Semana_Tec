dd753a35f316bf70c461b31b0957f0d00e02bef2
A01781835 <83768910+A01781835@users.noreply.github.com>
2021-05-06 11:00:03 -0500

    Update circulo

--- Python/paint.py
+++ Python/paint.py
@@ -35,7 +35,15 @@ def square(start, end):
 
 def circle(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    for count in range(10):
+        forward(end.x - start.x)
+        left(36)
+    end_fill()
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
